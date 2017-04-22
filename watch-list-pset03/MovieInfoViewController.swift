54cf1a1ff8501cf4968ae48ff6967dd69c5a4f96
majstomphorst <maxim.stomphorst@student.uva.nl>
2017-04-22 10:40:12 +0200

    hij verstuurd data maar laad de view 2x 1x fout 1x goed

---
+++ watch-list-pset03/MovieInfoViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MovieInfoViewController.swift
+//  watch-list-pset03
+//
+//  Created by Maxim Stomphorst on 21/04/2017.
+//  Copyright © 2017 M.a.j©. All rights reserved.
+//
+
+import UIKit
+
+class MovieInfoViewController: UIViewController {
+    
+    var test: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("MovieInfoViewC")
+        print(test)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- watch-list-pset03/ViewController.swift
+++ watch-list-pset03/ViewController.swift
@@ -8,23 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITabBarDelegate, UISearchBarDelegate {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var search: UISearchBar!
     
-    let url = URL(string: "https://www.omdbapi.com/?s=rogue")
-    
+    let url = URL(string: "https://www.omdbapi.com/?s=avatar")
     var items: [[String : AnyObject]] = []
+    var send = ""
     
-//    let mtitle = ["movie01", "movie02", "movie03"]
-//    
-//    let desc = [
-//        "movie01": "MOVIE01",
-//        "movie02": "MOVIE02",
-//        "movie03": "MOVIE03"
-//    ]
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,10 +26,10 @@ class ViewController: UIViewController, UITableViewDataSource, UITabBarDelegate,
                 print("getting json faild error")
             }
             else {
-                if let content = data
+                if let data = data
                 {
                     do {
-                        let json = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as! [String : AnyObject]
+                        let json = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as! [String : AnyObject]
                         let items = json["Search"] as! [[String : AnyObject]]
                         
                         self.items = items
@@ -59,13 +51,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITabBarDelegate,
     }
 
     
-    // search bar 
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        let keywords = search.text
-        print(keywords!)
-        
-    }
-    
+
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count
@@ -75,18 +61,69 @@ class ViewController: UIViewController, UITableViewDataSource, UITabBarDelegate,
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         as! MovieCell
-        
+    
         cell.imageUrl = URL(string: items[indexPath.row]["Poster"] as! String)
         cell.movieTitle.text = items[indexPath.row]["Title"] as? String
         cell.movieDescription.text = items[indexPath.row]["Year"] as? String
         
-        
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("you selectd row#\(indexPath)")
+        
+        send = items[indexPath.row]["Title"] as! String
+        print(type(of: send))
+        
+        // segue to the next controller
+        self.performSegue(withIdentifier: "movieInfoSegue", sender: "")
+    
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let movieInfoVC = segue.destination as? MovieInfoViewController {
+            movieInfoVC.test = send
+        }
     }
     
     
     
-    
+    //MARK: actions
+    // search bar
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        var search = self.search.text
+        print(search!)
+        
+        search = search?.addingPercentEncoding(withAllowedCharacters: .alphanumerics)
+        
+        let url = URL(string: "https://www.omdbapi.com/?s=\(search!)")
+        
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            if error != nil {
+                print("getting json faild error")
+            }
+            else {
+                if let data = data
+                {
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as! [String : AnyObject]
+                        let items = json["Search"] as! [[String : AnyObject]]
+                        
+                        self.items = items
+                        
+                        // process json -> mtitle
+                        DispatchQueue.main.async {
+                            self.tableView.reloadData()
+                        }
+                    }
+                    catch {
+                        print("error")
+                    }
+                }
+            }
+        }
+        task.resume()
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
