c5775aebe754d38e11436fba6b0481ab2961d7a8
Maj <maxim.stomphorst@student.uva.nl>
2017-04-26 12:04:17 +0200

    cleaing

--- watch-list-pset03/MovieCell.swift
+++ watch-list-pset03/MovieCell.swift
@@ -10,16 +10,25 @@ import UIKit
 
 class MovieCell: UITableViewCell {
     
+    @IBOutlet weak var movieImg: UIImageView!
+    @IBOutlet weak var movieTitle: UILabel!
+    @IBOutlet weak var movieDescription: UILabel!
+    
+    // if the value of imageUrl changes to the folowing
     var imageUrl: URL? {
         didSet {
+            // start a task on a second thread, go to the imageUrl and extracts image data
             let task = URLSession.shared.dataTask(with: imageUrl!) { (data, response, error) in
                 if error != nil {
                     print("getting img faild error")
                 } else {
                     if let data = data {
+                        // soring image data
                         let image = UIImage(data: data)
                         
+                        // on the main thread
                         DispatchQueue.main.async {
+                            // Assigning image data to Image placeholder
                             self.movieImg.image = image
                         }
                     }
@@ -27,20 +36,6 @@ class MovieCell: UITableViewCell {
             }
             task.resume()
         }
-    }
-
-    @IBOutlet weak var movieImg: UIImageView!
-    @IBOutlet weak var movieTitle: UILabel!
-    @IBOutlet weak var movieDescription: UILabel!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-        // Configure the view for the selected state
     }
 
 }
